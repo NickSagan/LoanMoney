3fd9baadbe9678db532d1e55fd8a0bb7bce24ca1
NickSagan <9952728@gmail.com>
2022-03-18 16:46:05 +0300

    add createBarButton()

--- LoanMoney/MainVC.swift
+++ LoanMoney/MainVC.swift
@@ -12,28 +12,55 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Займ"
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
-
-        let homeButton = UIBarButtonItem(title: "H", style: .plain, target: self, action: #selector(ProfileButtonTapped))
-
-        let flexibleSpace1 = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
-
-        let addButton = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(ProfileButtonTapped))
-
-        let flexibleSpace2 = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
-
-        let loveButton = UIBarButtonItem(title: "Love", style: .plain, target: self, action: #selector(ProfileButtonTapped))
-        
-        let flexibleSpace3 = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
-
-        let arr: [Any] = [flexibleSpace, homeButton, flexibleSpace1, addButton, flexibleSpace2, loveButton, flexibleSpace3]
-
-        setToolbarItems(arr as? [UIBarButtonItem] ?? [UIBarButtonItem](), animated: true)
-        navigationController?.setToolbarHidden(false, animated: false)
+        createToolbar()
     }
-
+    
     @objc func ProfileButtonTapped() {
-        
+        print("tapped")
+    }
+    
+}
+
+//MARK: - Toolbar & UIBarButtonItems
+
+extension MainVC {
+    
+    
+    func createBarButton(imageName: String, title: String, selector: Selector) -> UIBarButtonItem {
+        let customButton: UIButton = UIButton(type: .custom)
+        customButton.setImage((UIImage(named: imageName)), for: .normal)
+        customButton.setTitle(title, for: .normal)
+        customButton.titleLabel?.font = UIFont(name: "Verdana", size: 12)
+        customButton.setTitleColor(UIColor.black, for: .normal)
+        customButton.sizeToFit()
+        customButton.centerLabelVerticallyWithPadding(spacing: 5)
+        customButton.addTarget(self, action: selector, for: .touchUpInside)
+        return UIBarButtonItem(customView: customButton)
     }
 }
 
+extension UIButton {
+    func centerLabelVerticallyWithPadding(spacing:CGFloat) {
+        // update positioning of image and title
+        let imageSize = self.imageView!.frame.size
+        self.titleEdgeInsets = UIEdgeInsets(top:0,
+            left:-imageSize.width,
+            bottom:-(imageSize.height + spacing),
+            right:0)
+        let titleSize = self.titleLabel!.frame.size
+        self.imageEdgeInsets = UIEdgeInsets(top:-(titleSize.height + spacing),
+            left:0,
+            bottom: 0,
+            right:-titleSize.width)
+
+        // reset contentInset, so intrinsicContentSize() is still accurate
+        let trueContentSize = self.titleLabel!.frame.union(self.imageView!.frame).size
+        let oldContentSize = self.intrinsicContentSize
+        let heightDelta = trueContentSize.height - oldContentSize.height
+        let widthDelta = trueContentSize.width - oldContentSize.width
+        self.contentEdgeInsets = UIEdgeInsets(top:heightDelta/2.0,
+            left:widthDelta/2.0,
+            bottom:heightDelta/2.0,
+            right:widthDelta/2.0)
+    }
+}
