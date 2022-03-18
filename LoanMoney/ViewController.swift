84c36c078d494f3cea69396a39db16c286011332
NickSagan <9952728@gmail.com>
2022-03-18 14:56:16 +0300

    Initial Commit

---
+++ LoanMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
