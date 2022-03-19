0210defec416ff52da1db6aa67d9a0a0d45dc24d
NickSagan <9952728@gmail.com>
2022-03-19 20:53:47 +0300

    add class LoanCell

---
+++ LoanMoney/LoanCell.swift
@@ -0,0 +1,18 @@
+//
+//  LoanCell.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 19.03.2022.
+//
+
+import UIKit
+
+class LoanCell: UICollectionViewCell {
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
