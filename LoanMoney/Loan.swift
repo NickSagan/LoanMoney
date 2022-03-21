164a55bd6ae3b350025d9248898127df15c02979
NickSagan <9952728@gmail.com>
2022-03-21 21:00:06 +0300

    add Loan struct

---
+++ LoanMoney/Loan.swift
@@ -0,0 +1,16 @@
+//
+//  Loan.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 21.03.2022.
+//
+
+import Foundation
+
+struct Loan {
+    let logo: String
+    let percent: String
+    let blue: String
+    let violet: String
+    let yellow: String
+}
