dd4d61e75076eb808525c5b207de5620659829a8
NickSagan <9952728@gmail.com>
2022-03-21 21:09:33 +0300

    add Data struct

---
+++ LoanMoney/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 21.03.2022.
+//
+
+import Foundation
+
+struct Data {
+    let loans: [Loan] = [
+    Loan(logo: "smsfinance", percent: "под 0%", blue: "от 3000\nдо 30000", violet: "от 1\nдо 21 дня", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/0f3ad550-a214-11ec-b814-d97219863970")!)
+    ]
+}
