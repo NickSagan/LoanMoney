6906132eb7b361bf79669316b3d92bfa7149c9b4
NickSagan <9952728@gmail.com>
2022-03-22 16:10:38 +0300

    finish loans arr

--- LoanMoney/Data.swift
+++ LoanMoney/Data.swift
@@ -9,6 +9,17 @@ import Foundation
 
 struct Data {
     let loans: [Loan] = [
-    Loan(logo: "smsfinance", percent: "под 0%", blue: "от 3000\nдо 30000", violet: "от 1\nдо 21 дня", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/0f3ad550-a214-11ec-b814-d97219863970")!)
+    Loan(logo: "colibri", percent: "под 0%", blue: "от 2000\nдо 30000", violet: "от 1\nдо 30 дней", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/ffa71010-a214-11ec-8840-c901a67bae26")!),
+    Loan(logo: "smsfinance", percent: "под 0%", blue: "от 3000\nдо 30000", violet: "от 1\nдо 21 дня", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/0f3ad550-a214-11ec-b814-d97219863970")!),
+    Loan(logo: "migkredit", percent: "под 0%", blue: "от 3000\nдо 100000", violet: "от 5\nдо 21 дня", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/73308250-a216-11ec-a6e9-6f515a4495ea")!),
+    Loan(logo: "zaimbesplatnij", percent: "под 0%", blue: "от 1000\nдо 80000", violet: "от 5\nдо 21 дней", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/adcd2b90-a216-11ec-85c0-178a67aabdb3")!),
+    Loan(logo: "smartstart", percent: "под 0,1%", blue: "от 3000\nдо 30000", violet: "от 7\nдо 30 дней", yellow: "от 0,1%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/a20abf00-a6f8-11ec-a1ef-1f8a8681464b")!),
+    Loan(logo: "vivus", percent: "под 0%", blue: "от 3000\nдо 100000", violet: "от 1\nдо 52 дней", yellow: "от 0%\nдо 1% в день", url: URL(string: "https://my.saleads.pro/s/945a6070-a6f9-11ec-b059-4ff5e90a4559")!),
+    Loan(logo: "bistrodengi", percent: "под 30%", blue: "от 50000\nдо 1000000", violet: "от 3\nдо 36 мес", yellow: "от 30%\nдо 88% в год", url: URL(string: "https://my.saleads.pro/s/2ed65d60-a6fa-11ec-8531-df94dc920ba3")!),
+    Loan(logo: "horoshiedengi", percent: "под 1%", blue: "от 15000\nдо 50000", violet: "от 0\nдо 1 года", yellow: "от 1%\nдо 10% в день", url: URL(string: "https://my.saleads.pro/s/d8092070-a6fa-11ec-9cd9-5bd032211bbb")!),
+    Loan(logo: "srochnodengi", percent: "под 1%", blue: "от 2000\nдо 100000", violet: "от 1\nдо 30 дней", yellow: "1% в день", url: URL(string: "https://my.saleads.pro/s/dd5ccb10-a6fb-11ec-9e0b-9f4be1a0abbf")!),
+    Loan(logo: "dengisrazu", percent: "под 1%", blue: "от 1000\nдо 100000", violet: "от 16\nдо 180 дней", yellow: "от 1%\nдо 5% в день", url: URL(string: "https://my.saleads.pro/s/6e4ab540-a6fc-11ec-8544-e3980e4b26ce")!),
+    Loan(logo: "bistrodenginaputi", percent: "под 1%", blue: "от 1000\nдо 100000", violet: "от 7\nдо 180 дней", yellow: "от 1%\nдо 5% в день", url: URL(string: "https://my.saleads.pro/s/cdc22e30-a6fc-11ec-9a4b-fbb52bcccf89")!),
+    Loan(logo: "cashtoyou", percent: "под 1%", blue: "от 2000\nдо 30000", violet: "от 6\nдо 21 дня", yellow: "1% в день", url: URL(string: "https://my.saleads.pro/s/633f8920-a6fd-11ec-8b40-6bce582d5b30")!)
     ]
 }
