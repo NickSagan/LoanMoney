c78c814b9247f28dfd807f5363e2b34c6b79136b
NickSagan <9952728@gmail.com>
2022-03-22 18:33:35 +0300

    add CardFilterView class

---
+++ LoanMoney/Views/CardFilterView.swift
@@ -0,0 +1,19 @@
+//
+//  CardFilterView.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 22.03.2022.
+//
+
+import UIKit
+
+class CardFilterView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var cardsButton: UIButton!
+    
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+    }
+    @IBAction func cardsButton(_ sender: UIButton) {
+    }
+}
