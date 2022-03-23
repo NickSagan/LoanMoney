5fef678b782f35459a3aa5d24580f3fd7b256368
NickSagan <9952728@gmail.com>
2022-03-23 17:16:44 +0300

    add struct DebitCard

---
+++ LoanMoney/Models/DebitCard.swift
@@ -0,0 +1,17 @@
+//
+//  DebitCard.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 23.03.2022.
+//
+
+import Foundation
+
+struct DebitCard {
+    let image: String
+    let name: String
+    let percent: String
+    let blue: String
+    let violet: String
+    let url: URL
+}
