693f2078a1ad5a79e357e10d9ba2ab1db31c765c
NickSagan <9952728@gmail.com>
2022-03-23 17:15:18 +0300

    add struct CreditCard

---
+++ LoanMoney/Models/CreditCard.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCard.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 23.03.2022.
+//
+
+import Foundation
+
+struct CreditCard {
+    let image: String
+    let name: String
+    let percent: String
+    let blue: String
+    let violet: String
+    let url: URL
+}
