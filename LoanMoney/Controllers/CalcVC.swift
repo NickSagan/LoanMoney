99c38011e56fa252e8219da9079e99deea352012
NickSagan <9952728@gmail.com>
2022-03-23 16:48:12 +0300

    add empty CalcVC, InfoVC

---
+++ LoanMoney/Controllers/CalcVC.swift
@@ -0,0 +1,12 @@
+//
+//  CalcVC.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 23.03.2022.
+//
+
+import UIKit
+
+class CalcVC: UIViewController {
+    
+}
---
+++ LoanMoney/Controllers/InfoVC.swift
@@ -0,0 +1,12 @@
+//
+//  InfoVC.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 23.03.2022.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+    
+}
