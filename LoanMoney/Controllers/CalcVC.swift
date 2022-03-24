4d97fb3d58d84dc19895d74e9d6601a8c715e430
NickSagan <9952728@gmail.com>
2022-03-24 11:18:12 +0300

    finish CalcVC

--- LoanMoney/Controllers/CalcVC.swift
+++ LoanMoney/Controllers/CalcVC.swift
@@ -6,7 +6,29 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalcVC: UIViewController {
     
+    var calculatorView: CalculatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Калькулятор"
+        calculatorView = CalculatorView()
+        view.addSubview(calculatorView)
+        calculatorView.translatesAutoresizingMaskIntoConstraints = false
+        calculatorView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top)
+            make.bottom.equalTo(view.snp.bottom)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+        }
+        calculatorView.yellowButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc func buttonTapped() {
+        let vc = LoansVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
