45bf1884f2ac86040f461418159ae0e0a7d0406e
NickSagan <9952728@gmail.com>
2022-03-24 12:19:22 +0300

    finish InfoVC

--- LoanMoney/Controllers/InfoVC.swift
+++ LoanMoney/Controllers/InfoVC.swift
@@ -8,5 +8,30 @@
 import UIKit
 
 class InfoVC: UIViewController {
+
+    let info: UITextView = {
+        let tv = UITextView()
+        tv.textColor = .black
+        tv.font = UIFont(name: "verdana", size: 14)
+        tv.isEditable = false
+        return tv
+    }()
     
+    let data = Data()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Информация"
+        info.text = data.infotext
+
+        view.addSubview(info)
+        info.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            info.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            info.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            info.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            info.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+        ])
+    }
 }
