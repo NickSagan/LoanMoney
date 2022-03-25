d1cf1f961ae7f5a5a8061a4249017a9be783c36b
NickSagan <9952728@gmail.com>
2022-03-25 15:24:08 +0300

    finish NewsVC

--- LoanMoney/Controllers/NewsVC.swift
+++ LoanMoney/Controllers/NewsVC.swift
@@ -5,34 +5,78 @@
 //  Created by Nick Sagan on 25.03.2022.
 //
 
+//
+//  NewsVC.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 25.03.2022.
+//
+
 import UIKit
 
-class NewsVC: UICollectionViewController {
+class NewsVC: UIViewController {
+
+    private var refreshControl: UIRefreshControl!
+    private var collectionView: UICollectionView!
+    let data = Data()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.collectionView!.register(NewsCell.self, forCellWithReuseIdentifier: "cell")
-
-
+        title = "Новости"
+        setupCollectionView()
     }
+}
 
-    // MARK: UICollectionViewDataSource
+//MARK: - COLLECTION VIEW SETUP
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 0
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+extension NewsVC {
     
-        // Configure the cell
+    func setupCollectionView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: setLayout())
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        collectionView?.register(NewsCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView?.backgroundColor = UIColor.white
+        
+        refreshControl = UIRefreshControl()
+        collectionView?.refreshControl = refreshControl
+        refreshControl?.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
+        
+        view.addSubview(collectionView!)
+        self.view = view
+    }
     
+    func setLayout() -> UICollectionViewFlowLayout {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        layout.itemSize.width = self.view.frame.width - 10
+        layout.itemSize.height = 450
+        return layout
+    }
+    
+    @objc func refresh(_ sender: AnyObject) {
+        usleep(UInt32(0.5))
+        refreshControl.endRefreshing()
+    }
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension NewsVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.news.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewsCell
+        cell.imageNews.image = UIImage(named: data.news[indexPath.row].image)
+        cell.titleNews.text = data.news[indexPath.row].title
+        cell.textNews.text = data.news[indexPath.row].text
         return cell
     }
 }
