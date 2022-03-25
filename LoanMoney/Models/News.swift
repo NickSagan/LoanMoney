aba8b5c56046c6999056c5639119bae28dce561b
NickSagan <9952728@gmail.com>
2022-03-25 14:10:09 +0300

    add news struct

---
+++ LoanMoney/Models/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 25.03.2022.
+//
+
+import Foundation
+
+struct News {
+    let image: String
+    let title: String
+    let text: String
+}
