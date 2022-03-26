0beca30d68f3f9047f33d260bb63a2f0a76828ac
NickSagan <9952728@gmail.com>
2022-03-26 13:11:28 +0300

    add CompaniesVC

---
+++ LoanMoney/Controllers/CompaniesVC.swift
@@ -0,0 +1,43 @@
+//
+//  CompaniesVC.swift
+//  LoanMoney
+//
+//  Created by Nick Sagan on 26.03.2022.
+//
+
+import UIKit
+
+class CompaniesVC: UITableViewController {
+    
+    let data = Data()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.sideMenu.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data.sideMenu[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0: UIApplication.shared.open(URL(string: "https://credistory.ru/")!, options: [:])
+        case 1: let vc = NewsVC(); navigationController?.pushViewController(vc, animated: true)
+        case 2: tabBarController?.selectedIndex = 2
+        case 3: tabBarController?.selectedIndex = 3
+        default: print("Error: row out of index")
+        }
+
+    }
+}
